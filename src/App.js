94128b3c0aefc1b1f891626ee6d4ce2511e4e4de
sikorskayaX <sikorskaya02@gmail.com>
2023-12-05 02:33:02 +0300

    Add PostItem and PostList components
    This commit adds the PostItem and PostList components to the project. The PostItem component displays a single post with a delete button, while the PostList component displays a list of posts with a title.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import PostItem from './components/PostItem';
+import './styles/App.css';
+import PostList from './components/PostList';
 
 
 const App = () => {
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Javascript 1', body: 'Javascript - programming language'},
+    {id: 2, title: 'Javascript 2', body: 'Javascript - programming language'},
+    {id: 3, title: 'Javascript 3', body: 'Javascript - programming language'}
+  ])
+  const [posts2, setPosts2] = useState([
+    {id: 1, title: 'Python 1', body: 'Python - programming language'},
+    {id: 2, title: 'Python 2', body: 'Python - programming language'},
+    {id: 3, title: 'Python 3', body: 'Python - programming language'}
+  ])
   return (
-    <h1>Hello, world!</h1>,
-    <p>It's react templates</p>
+    <div className="App">
+        <PostList posts={posts} title="Posts about JS"/>
+        <PostList posts={posts2} title="Posts about Python"/>
+    </div>
   );
 }
+
 
 export default App;
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,21 @@
+﻿import React, {useState} from 'react';
+
+const PostItem = (props) => {
+
+
+  return (
+    <div className = "post">
+    <div className = "post__content">
+      <strong>{props.post.id}. {props.post.title}</strong>
+      <div>
+        {props.post.body}
+      </div>
+    </div>
+    <div className='post__btns'>
+          <button>Delete</button>
+    </div>
+  </div>
+  );
+};
+
+export default PostItem;
