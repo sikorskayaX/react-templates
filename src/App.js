1f27e3eb6ba6d0f0bf1b6e5d034281fda41651ac
sikorskayaX <sikorskaya02@gmail.com>
2023-12-06 20:48:22 +0300

    Refactor App.js and add PostForm component
    - Removed unnecessary imports and components from App.js
    - Created a new PostForm component to handle the form for creating new posts
    - Updated App.js to use the PostForm component instead of the previous form implementation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, {useState} from 'react';
-import PostItem from './components/PostItem';
 import './styles/App.css';
 import PostList from './components/PostList';
-import MyButton from './components/UI/button/MyButton';
-import MyInput from './components/UI/input/MyInput';
+import PostForm from './components/PostForm';
 
 
 const App = () => {
@@ -13,31 +11,13 @@ const App = () => {
     {id: 3, title: 'Javascript 3', body: 'Javascript - programming language'}
   ])
 
-  // способ получить инпут (управляемый элемент)
-  const [post, setPost] = useState({title: '', body: ''})
-
-  const addNewPost = (e)=>{
-    e.preventDefault()
-    setPosts([...posts, {...post, id: Date.now()}])
-    setPost({title: '', body: ''})
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
   }
 
   return (
     <div className="App">
-      <form>
-        {/*Управляемые компоненты*/}
-        <MyInput 
-          value = {post.title}
-          onChange = {e => setPost({...post, title: e.target.value})}
-          type='text' 
-          placeholder ='Post header'/>
-        <MyInput 
-          value = {post.body}
-          onChange = {e => setPost({...post, body: e.target.value})}
-          type='text' 
-          placeholder ='Post body'/>
-        <MyButton onClick = {addNewPost}>Create post</MyButton>
-      </form>
+        <PostForm create = {createPost}/>
         <PostList posts={posts} title="Posts about JS"/>
     </div>
   );
