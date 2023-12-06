c93bc4e65ab6ed37209a252e4d4e8ce0e44a190c
sikorskayaX <sikorskaya02@gmail.com>
2023-12-06 19:21:57 +0300

    Add input and button components for creating new posts
    - Added MyButton component for creating buttons with custom styles
    - Added MyInput component for creating input fields with custom styles
    - Implemented functionality to add new posts with user input

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react';
 import PostItem from './components/PostItem';
 import './styles/App.css';
 import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 
 
 const App = () => {
@@ -10,15 +12,40 @@ const App = () => {
     {id: 2, title: 'Javascript 2', body: 'Javascript - programming language'},
     {id: 3, title: 'Javascript 3', body: 'Javascript - programming language'}
   ])
-  const [posts2, setPosts2] = useState([
-    {id: 1, title: 'Python 1', body: 'Python - programming language'},
-    {id: 2, title: 'Python 2', body: 'Python - programming language'},
-    {id: 3, title: 'Python 3', body: 'Python - programming language'}
-  ])
+
+  // способ получить инпут (управляемый элемент)
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const addNewPost = (e)=>{
+    e.preventDefault()
+    const newPost ={
+      id: Date.now(),
+      title, 
+      body
+    }
+    setPosts([...posts, newPost])
+    setTitle('')
+    setBody('')
+  }
+
   return (
     <div className="App">
+      <form>
+        {/*Управляемые компоненты*/}
+        <MyInput 
+          value = {title}
+          onChange = {e => setTitle(e.target.value)}
+          type='text' 
+          placeholder ='Post header'/>
+        <MyInput 
+          value = {body}
+          onChange = {e => setBody(e.target.value)}
+          type='text' 
+          placeholder ='Post body'/>
+        <MyButton onClick = {addNewPost}>Create post</MyButton>
+      </form>
         <PostList posts={posts} title="Posts about JS"/>
-        <PostList posts={posts2} title="Posts about Python"/>
     </div>
   );
 }
