c19d8c4ebce271ba1ed5f39b5f7a42d8ef6b3290
sikorskayaX <sikorskaya02@gmail.com>
2023-12-06 20:36:12 +0300

    Refactor App component to use a single state object for post inputs

--- src/App.js
+++ src/App.js
@@ -14,19 +14,12 @@ const App = () => {
   ])
 
   // способ получить инпут (управляемый элемент)
-  const [title, setTitle] = useState('')
-  const [body, setBody] = useState('')
+  const [post, setPost] = useState({title: '', body: ''})
 
   const addNewPost = (e)=>{
     e.preventDefault()
-    const newPost ={
-      id: Date.now(),
-      title, 
-      body
-    }
-    setPosts([...posts, newPost])
-    setTitle('')
-    setBody('')
+    setPosts([...posts, {...post, id: Date.now()}])
+    setPost({title: '', body: ''})
   }
 
   return (
@@ -34,13 +27,13 @@ const App = () => {
       <form>
         {/*Управляемые компоненты*/}
         <MyInput 
-          value = {title}
-          onChange = {e => setTitle(e.target.value)}
+          value = {post.title}
+          onChange = {e => setPost({...post, title: e.target.value})}
           type='text' 
           placeholder ='Post header'/>
         <MyInput 
-          value = {body}
-          onChange = {e => setBody(e.target.value)}
+          value = {post.body}
+          onChange = {e => setPost({...post, body: e.target.value})}
           type='text' 
           placeholder ='Post body'/>
         <MyButton onClick = {addNewPost}>Create post</MyButton>
